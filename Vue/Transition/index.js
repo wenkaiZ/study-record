e047d7cd26bcf9145315783515e23ab3d3e64468
wenkaiZ <zhngwnk@outlook.com>
2019-06-28 10:17:13 +0800

    Vue basis 3.0

---
+++ Vue/Transition/index.js
@@ -0,0 +1,27 @@
+new Vue({
+    el: '#demo',
+    data: {
+      show: true
+    }
+  })
+
+  new Vue({
+    el: '#example-1',
+    data: {
+      show: true
+    }
+  })
+
+  new Vue({
+    el: '#example-2',
+    data: {
+      show: true
+    }
+  })
+
+  new Vue({
+    el: '#example-3',
+    data: {
+      show: true
+    }
+  })
